c09948c01cb317c83dbb471d26d8e696cb7c0fd9
Maryam Hokmabadi <57521340+MrymHkmbdi@users.noreply.github.com>
2022-01-20 22:55:05 +0330

    a little clean up on dataset

--- Final Project/collaborative.py
+++ Final Project/collaborative.py
@@ -5,3 +5,16 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 anime = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/irm-final-dataset/anime.csv")
 rating = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/irm-final-dataset/rating.csv")
+
+anime_ratings_frame = pd.DataFrame(rating.groupby('anime_id')['rating'].count())
+anime_ratings_frame = anime_ratings_frame[anime_ratings_frame.rating >= 1000]
+
+user_rating_counts_frame = pd.DataFrame(rating.groupby('user_id')['rating'].count())
+user_rating_counts_frame = user_rating_counts_frame[user_rating_counts_frame.rating >= 200]
+
+reduced_frame = rating[rating.anime_id.isin(anime_ratings_frame.index.tolist())]
+reduced_frame = rating[rating.anime_id.isin(user_rating_counts_frame.index.tolist())]
+
+reduced_frame = reduced_frame.replace(-1, np.nan)
+
+
